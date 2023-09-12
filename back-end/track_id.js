170ec4f2eb4243c3c81a98f241091efc277969f6
dawid2k20 <dawid.ordon2000@gmail.com>
2023-09-12 14:31:52 +0200

    Create track_id.js
    Wstępna walidacja track id

---
+++ back-end/track_id.js
@@ -0,0 +1,29 @@
+import express from "express";
+//testy
+console.log(get_id("7ABLbnD53cQK00mhcaOUVG"))
+console.log(get_id("https://open.spotify.com/track/7ABLbnD53cQK00mhcaOUVG"))
+console.log(get_id("prywatny bal"))
+
+async function get_id(value)
+{
+    //sprawdzenie czy podany ciąg jest id poprzez weryfikacje długości oraz czy zawiera spacje
+    if(value.indexOf(" ")==-1&&value.length==22)
+    {
+        return(value)
+    }
+    else
+    {
+        //dzielenie ciągu na tablice
+        let string = value.split("/");
+        //weryfikacja czy link jest prawidłowy
+        if(string[3]=="track"&&string[4].length==22)
+        {
+            return(string['4'])
+        }
+        //dany ciąg nie pasuje do kryteriów przez co prawdopodobnie jest to tytuł 
+        else
+        {
+            return(false)
+        }
+    }
+}
