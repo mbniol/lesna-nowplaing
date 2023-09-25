94355fe577540bdc5fb0293c1dc7c75758cb78ad
dawid2k20 <dawid.ordon2000@gmail.com>
2023-09-25 20:45:44 +0200

    vote update
    Dodano funkcje od głosowania i pobierania wyników

--- models/song.js
+++ models/song.js
@@ -1,45 +1,51 @@
-import Mysql from "../helpers/database.js";
+//import Mysql from "../helpers/database.js";
 import { fetchWebApi } from "../helpers/helpers.js";
 
-const pool = Mysql.getPromiseInstance();
-
-export async function vote(token, track_id) {
-  const track = await fetchWebApi(token, "tracks/" + track_id);
-  //piosenka jest niecenzuralna
-  if (track["explicit"] == true) {
-    return "piosenka jest nieodpowiednia";
-  } else {
-    //pobranie danych o piosence z bazy
-    const [rows] = await pool.query(
-      `
-        SELECT count(*) as count, banned
-        FROM tracks 
-        where id=?`,
-      [track_id]
-    );
-    //piosenka została zbanowana przez admina
-    if (rows[0]["banned"] == 1) {
-      return "piosenka zostala zabanowan przez administracje";
-    }
-    //piosenki nie ma w bazie danych
-    else if (rows[0]["count"] == 0) {
-      newTruck(
-        track_id,
-        track["album"]["images"][0]["url"],
-        track["duration_ms"],
-        track["name"]
-      );
-      addVote(track_id);
-      return "dodano piosenkę i głos";
+export async function vote(Mysql, token, track_link) {
+  //przeksztalcenie linku na track id
+  const track_id = await get_id(track_link);
+  if (track_id) {
+    const track = await fetchWebApi(token, "tracks/" + track_id);
+    if (track["error"] != undefined) {
+      console.log("wystapil blad przy odczycie piosenki");
     } else {
-      addVote(track_id);
-      return "dodano głos";
+      //piosenka jest niecenzuralna
+      if (track["explicit"] == true) {
+        return "piosenka jest nieodpowiednia";
+      } else {
+        //pobranie danych o piosence z bazy
+        const rows = await Mysql.executeQuery(
+          `SELECT count(*) as count, banned FROM tracks where id=?`,
+          [track_id]
+        );
+        //piosenka została zbanowana przez admina
+        if (rows[0]["banned"] == 1) {
+          return "piosenka zostala zabanowan przez administracje";
+        }
+        //piosenki nie ma w bazie danych
+        else if (rows[0]["count"] == 0) {
+          newTruck(
+            Mysql,
+            track_id,
+            track["album"]["images"][0]["url"],
+            track["duration_ms"],
+            track["name"]
+          );
+          addVote(Mysql, track_id);
+          return "dodano piosenkę i głos";
+        } else {
+          addVote(Mysql, track_id);
+          return "dodano głos";
+        }
+      }
     }
+  } else {
+    return "podany link jest nieprawidłowy";
   }
 }
 //dodanie nowej piosenki do bazy danych
-export async function newTruck(id, cover, length, name) {
-  await pool.query(
+export async function newTruck(Mysql, id, cover, length, name) {
+  await Mysql.executeQuery(
     `
     INSERT INTO tracks (id, cover, length, name, banned) VALUES (?, ?, ?, ?, ?)
     `,
@@ -47,8 +53,8 @@ export async function newTruck(id, cover, length, name) {
   );
 }
 //dodanie głosu na pisonekę z aktualną datą
-export async function addVote(id) {
-  await pool.query(
+export async function addVote(Mysql, id) {
+  await Mysql.executeQuery(
     `
     INSERT INTO votes (id, track_id, date_added) VALUES (NULL, ?, current_timestamp())
     `,
@@ -56,27 +62,52 @@ export async function addVote(id) {
   );
 }
 //sprawdzanie aktualnej listy głosów
-// export async function votes() {
-//   //zapytanie co wyciąga sume głosów z danego dnia i połowę glosów z dnia poprzedniego
-//   const [rows] = await pool.query(`
-//     SELECT tracks.name, count(main_votes.id)+
-// 	(if(/*sprawdzenie czy wartość połowy głosów nie jest null*/
-//         (SELECT count(votes.id)/2 as count
-//         FROM votes join tracks on votes.track_id = tracks.id 
-//         WHERE votes.date_added=CURRENT_DATE()-1 AND main_votes.track_id=votes.track_id
-//         group by tracks.id),/*jeżeli nie jest null zwróć liczbę głosów*/
-//         (SELECT floor(count(votes.id)/2) as count
-//         FROM votes join tracks on votes.track_id = tracks.id 
-//         WHERE votes.date_added=CURRENT_DATE()-1
-//         group by tracks.id),/*jeżeli jest null zwróć 0*/
-//         0)) 
-//     as count
-//     FROM votes as main_votes join tracks on main_votes.track_id = tracks.id
-//     WHERE main_votes.date_added=CURRENT_DATE()
-//     group by tracks.id
-//     order by count DESC;`);
-//   //wypisanie listy głosów
-//   rows.forEach((element) => {
-//     console.log(element["name"] + " " + element["count"]);
-//   });
-// }
+export async function votes(Mysql) {
+  //zapytanie co wyciąga sume głosów z danego dnia i połowę glosów z dnia poprzedniego
+  try {
+    const result = await Mysql.executeQuery(`
+    SELECT tracks.id, tracks.cover, tracks.name, count(main_votes.id)+
+      (if(/*sprawdzenie czy wartość połowy głosów nie jest null*/
+        (SELECT count(votes.id)/2 as count
+        FROM votes join tracks on votes.track_id = tracks.id 
+        WHERE votes.date_added=CURRENT_DATE()-1 AND main_votes.track_id=votes.track_id
+        group by tracks.id),/*jeżeli nie jest null zwróć liczbę głosów*/
+        (SELECT floor(count(votes.id)/2) as count
+        FROM votes join tracks on votes.track_id = tracks.id 
+        WHERE votes.date_added=CURRENT_DATE()-1
+        group by tracks.id),/*jeżeli jest null zwróć 0*/
+        0)) 
+    as count
+    FROM votes as main_votes join tracks on main_votes.track_id = tracks.id
+    WHERE main_votes.date_added=CURRENT_DATE()
+    group by tracks.id
+    order by count DESC;`);
+    //console.log(result);
+    return result;
+  } catch (error) {
+    console.error("Błąd zapytania:", error);
+    return false;
+  }
+
+  /*   //wypisanie listy głosów
+  rows.forEach((element) => {
+    console.log(element["name"] + " " + element["count"]);
+  }); */
+}
+async function get_id(value) {
+  //sprawdzenie czy podany ciąg jest id poprzez weryfikacje długości oraz czy zawiera spacje
+  if (value.indexOf(" ") == -1 && value.length == 22) {
+    return value;
+  } else {
+    //dzielenie ciągu na tablice
+    let string = value.split("/");
+    //weryfikacja czy link jest prawidłowy
+    if (string[3] == "track" && string[4].length == 22) {
+      return string["4"];
+    }
+    //dany ciąg nie pasuje do kryteriów przez co prawdopodobnie jest to tytuł
+    else {
+      return false;
+    }
+  }
+}
