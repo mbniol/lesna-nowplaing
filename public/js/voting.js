fd19371bc3af3cc3afbbfc48d6c1174ab785ac93
canicjusz <janek.bananek371@gmail.com>
2023-09-26 10:32:11 +0200

    voting strucutre fix

--- public/js/voting.js
+++ public/js/voting.js
@@ -1,7 +1,7 @@
 const closeBtn = document.getElementById("closeBtn");
 const openBtn = document.getElementsByClassName("add-song-floating-btn")[0];
 const addSongPanel = document.getElementsByClassName("add-song")[0];
-const addSongBtn = document.getElementsById("addSongBtn");
+const addSongBtn = document.getElementById("addSongBtn");
 
 openBtn.addEventListener("click", async (e) => {
   addSongPanel.classList.add("visible");
