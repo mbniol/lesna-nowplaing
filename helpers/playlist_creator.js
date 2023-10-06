3264426e2738ccd9bd28d6180db48ca567d39238
dawid2k20 <dawid.ordon2000@gmail.com>
2023-10-06 07:35:22 +0200

    prace nad playlistami

--- helpers/playlist_creator.js
+++ helpers/playlist_creator.js
@@ -19,4 +19,19 @@ export async function make_playlist(token) {
     track_list
   );
   console.log(track_list);
+  const insert={
+    'uris':[
+      'spotify:track:0AUyNF6iFxMNQsNx2nhtrw',
+      'spotify:track:54ipXppHLA8U4yqpOFTUhr'
+    ],
+    'position': 0
+  }
+  const test_insert = await fetchWebApi(
+      token,
+      "playlists/3sCbvsgOGlqZXaLQ9X9R1U/tracks",
+      "POST",
+      insert
+  );
+  console.log(test_insert);
+
 }
