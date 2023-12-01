054a1939a15de3c49a7884301db28e2de6e2c78c
canicjusz <janek.bananek371@gmail.com>
2023-12-01 18:25:45 +0100

    voteowanie bez sprawdzania ip

--- models/auth.js
+++ models/auth.js
@@ -2,22 +2,21 @@ import Mysql from "../helpers/database.js";
 import { errorHandler } from "../helpers/errorHandler.js";
 
 export default class Model {
-  static async checkTodaysVote(id, visitorId) {
+  static async checkTodaysVote(visitorId) {
     const pool = Mysql.getPromiseInstance();
     const [result, err] = await errorHandler(
       pool.query,
       pool,
       `
       SELECT 
-      EXISTS(SELECT date_added = CURDATE() FROM votes WHERE ip = ? OR visitor_id=?)
+      EXISTS(SELECT date_added = CURDATE() FROM votes WHERE visitor_id=?)
       as has_voted
     `,
-      [id, visitorId]
+      [visitorId]
     );
     if (err) {
       throw new Error("Nie udało isę wykonać zapytania", { cause: err });
     }
-    console.log(result, id);
     return result[0][0].has_voted;
   }
 }
